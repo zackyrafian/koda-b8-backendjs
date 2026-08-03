9d8b58543b5c19bd2a61f52d555d2609580450b9
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-03 21:22:07 +0700

    feat: login route, controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
-import argon2, { hash }  from "argon2"
+import argon2, { hash } from "argon2"
+import libJWT from "../libs/jwt.js";
 import * as userModel from "../models/user.model.js"
 
 export async function register(req, res) { 
@@ -32,6 +33,48 @@ export async function register(req, res) {
     res.status(500).json({ 
       "success": false, 
       "message": error.message,
+    })
+  }
+}
+
+export async function loign(req, res) { 
+  try { 
+    const { email, password } = req.body; 
+    if (!email || !password) {
+       return res.status(400).json({ 
+        "success": false, 
+        "message": "Required email & password"
+      })
+    }
+    
+    const user = await userModel.findOne("email", email)
+    if (!user) { 
+      res.status(400).json({ 
+        "success": false, 
+        "message": "We don't have your email."
+      })
+      return
+    }
+
+    const valid = await argon2.verify(user.password, password)
+    if (!valid) { 
+      return res.status(401).json({
+        "success": false, 
+        "message": "Email or password wrong."
+      })
+    }
+
+    const token = libJWT.sign({ 
+      userId: user.id
+    })
+    res.status(200).json({ 
+      "success": true,
+      "data": { token }
+    })
+  } catch (error) { 
+    res.status(500).json({ 
+      "success": false, 
+      "message": error.message
     })
   }
 }
--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import * as authController from "../controllers/auth.controller.js"
 const authRouter = Router();
 authRouter.post("/register", authController.register)
-// authRouter.post("/login")
+authRouter.post("/login", authController.loign)
 
 export default authRouter;
