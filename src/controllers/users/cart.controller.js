656ad4c1fd6b0af4745ffa59f7d07635181bf1fa
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-04 22:20:24 +0700

    feat: add getAll to cart controller

--- src/controllers/users/cart.controller.js
+++ src/controllers/users/cart.controller.js
@@ -1,5 +1,28 @@
 import * as userCartModel from "../../models/users/cart.model.js"
 
+export async function getAll(req, res) { 
+  const user_id = parseInt(req.user.userId)
+  try { 
+    const carts = await userCartModel.findAll(user_id)
+    if (!carts) { 
+      res.status(404).json({ 
+        "success": false, 
+        "message": `User id ${user_id} not found.`
+      })
+      return
+    }
+    res.status(200).json({ 
+      "success": true, 
+      "results": carts
+    })
+  } catch (error) { 
+    res.status(500).json({ 
+      "success": true, 
+      "message": error.message
+    })
+  }
+}
+
 export async function create(req, res) { 
   const user_id = parseInt(req.user.userId)
   const { product_id, quantity } = req.body;
