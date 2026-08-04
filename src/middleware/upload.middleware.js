9c02556d3b3a649485625115c903f9476bf22317
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-04 17:20:13 +0700

    feat: add upload middleware

---
+++ src/middleware/upload.middleware.js
@@ -0,0 +1,32 @@
+import multer from "multer"
+
+const storage = multer.diskStorage({ 
+  destination: (req, file, callback) => { 
+    callback(null, 'uploads')
+  }, 
+  filename: (req, file, callback) => { 
+    callback(null, file.originalname)
+  }
+})
+
+const upload = multer({ 
+  storage: storage, 
+  limits: {fileSize: 2 * 1024 * 1024}
+})
+
+function uploadMiddleware(fieldName) {
+  return (req, res, next) => { 
+    upload.single(fieldName)(req, res, (error) => { 
+      if (error) { 
+        res.status(400).json({ 
+          "success": false, 
+          "message": error.message
+        })
+        return
+      }
+      next()
+    })
+  }
+}
+
+export default uploadMiddleware
