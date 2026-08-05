b71ca9af80d4c1829749c975747f8dbeecf8cd56
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-06 04:59:16 +0700

    refactor: use JOIN with brands and categories in products query

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import brandRouter from "./brands.router.js"
 import categoriesRouter from "./categories.router.js"
 import usersRouter from "./users.router.js"
 import productsRouter from "./products.router.js"
+import paymentRouter from "./payment.router.js"
 
 
 const router = Router() 
@@ -12,4 +13,5 @@ router.use("/brands", brandRouter)
 router.use("/categories", categoriesRouter)
 router.use('/users', usersRouter)
 router.use('/products', productsRouter)
+router.use('/payments', paymentRouter)
 export default router
