0bb2dd3294fccc4d7c0a578766718a02f40542e4
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-07 11:48:13 +0700

    fix: change upload destination folder to images

--- src/middleware/upload.middleware.js
+++ src/middleware/upload.middleware.js
@@ -2,7 +2,7 @@ import multer from "multer"
 
 const storage = multer.diskStorage({ 
   destination: (req, file, callback) => { 
-    callback(null, 'uploads')
+    callback(null, ('images'))
   }, 
   filename: (req, file, callback) => { 
     callback(null, file.originalname)
