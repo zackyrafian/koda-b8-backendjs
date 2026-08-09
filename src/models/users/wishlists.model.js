14ccb93db1a24289fcd60aa24c4dfc6f478c84ce
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-10 00:31:37 +0700

    fix: update wishlists model to use correct table and remove quantity field

--- src/models/users/wishlists.model.js
+++ src/models/users/wishlists.model.js
@@ -1,19 +1,19 @@
 import { pool } from "../../config/postgres.js";
 
-export async function create(user_id, product_id, quantity) { 
+export async function create(user_id, product_id) { 
   const { rows } = await pool.query(
-    `INSERT INTO user_wishlists (user_id, product_id, quantity, created_at, updated_at) VALUES ($1, $2, $3, NOW(), NOW()) RETURNING * `, [user_id, product_id, quantity]
+    `INSERT INTO user_wishlists (user_id, product_id, created_at) VALUES ($1, $2, NOW()) RETURNING * `, [user_id, product_id]
   )
   return rows[0];
 }
 
 export async function findAll(user_id) { 
   const { rows } = await pool.query(
-    `SELECT uc.id, uc.user_id, uc.product_id, uc.quantity, uc.created_at, uc.updated_at, json_build_object(
+    `SELECT uc.id, uc.user_id, uc.product_id, uc.created_at, json_build_object(
     'id', p.id,
     'name', p.name,
     'price', p.price
-    ) as product FROM user_carts as uc
+    ) as product FROM user_wishlists as uc
     JOIN products as p ON uc.product_id = p.id
     WHERE user_id = $1 ORDER BY uc.created_at DESC`, [user_id]
   )
