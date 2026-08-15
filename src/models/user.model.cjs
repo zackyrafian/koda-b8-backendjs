0c0a2017bd23c6ad24833c5824d0c6aab6fc4957
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-15 22:12:47 +0700

    refactor: get userProfile use sequelize

---
+++ src/models/user.model.cjs
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      User.hasOne(models.UserProfile, { 
+        foreignKey: 'user_id', 
+        as: 'profile'
+      })
+    }
+  }
+  User.init({
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users',
+    underscored: true,
+  });
+  return User;
+};
