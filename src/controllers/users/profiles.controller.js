53b601fb217fb8ad06fea1ae70fcd6229adbf527
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-15 22:39:44 +0700

    refactor: user profile & user update

--- src/controllers/users/profiles.controller.js
+++ src/controllers/users/profiles.controller.js
@@ -1,20 +1,41 @@
 import * as userModel from "../../models/users/profiles.model.js"
+import sq from '../../models/index.js'
 
 export async function getUserProfile(req, res) { 
   const user_id = parseInt(req.user.userId)
-  const user = await userModel.findProfile(user_id)
+  // const user = await userModel.findProfile(user_id)
   try { 
-    if (!user) { 
-      res.status(404).json({ 
-        "success": false, 
-        "message": `404 -> ${user_id}`
-      })
-      return;
+    const user = await sq.User.findByPk(user_id, {
+      attributes: { 
+        exclude: ["password"]
+      }, 
+      include: [{ 
+        model: sq.UserProfile, 
+        as: 'profile'
+      }]
+    })
+
+    const response = {
+      ...user.toJSON(),
+      fullname: user.profile?.fullname || null, 
+      phone_number: user.profile?.phone_number || null,
+      gender: user.profile?.gender || null,
+      date_of_birth: user.profile?.date_of_birth || null,
+      image_profile: user.profile?.image_profile || null,
+      profile: undefined
     }
+    
+    // if (!user) {
+    //   res.status(404).json({
+    //     "success": false,
+    //     "message": `404 -> ${user_id}`
+    //   })
+    //   return;
+    // }
     res.status(200).json({ 
-      "status": true, 
-      "message": "Successfully get users profile",
-      "result": user
+      status: true, 
+      message: "Successfully get users profile",
+      result: response
     })
   } catch (error) { 
     res.status(500).json({ 
@@ -28,18 +49,30 @@ export async function profileImage(req, res) {
   const user_id = parseInt(req.user.userId)
   const { file } = req 
   const { body } = req; 
-  // console.log(req.file)
-  // console.log(req.body)
-  console.log(body)
   try { 
-    await userModel.updateProfile(user_id, {
-      fullname: body.full_name,
-      email: body.email, 
-      phone_number: body.phone_number,
-      date_of_birth: body.date_of_birth, 
-      gender: body.gender,
-      picture_path: file?.path,
-    })
+    // await userModel.updateProfile(user_id, {
+    //   fullname: body.full_name,
+    //   email: body.email,
+    //   phone_number: body.phone_number,
+    //   date_of_birth: body.date_of_birth,
+    //   gender: body.gender,
+    //   picture_path: file?.path,
+    // })
+
+      await sq.User.update(
+        { email: body.email },
+        { where: { id: user_id } }
+      )
+  
+      await sq.UserProfile.update({
+        fullname: body.full_name,
+        phone_number: body.phone_number,
+        date_of_birth: body.date_of_birth,
+        gender: body.gender,
+        image_profile: file?.path
+      }, {
+        where: { user_id }
+      })
 
     res.status(200).json({ 
       "success": true, 
