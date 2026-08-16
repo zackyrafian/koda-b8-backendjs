fb40c46620be2b66fa102725471c7ecd988d5166
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-16 19:32:10 +0700

    feat: payments model sequelize

---
+++ src/models/payments.model.cjs
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Payments extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Payments.init({
+    order_id: DataTypes.INTEGER,
+    payment_method_id: DataTypes.INTEGER,
+    va_number: DataTypes.STRING,
+    amount: DataTypes.DECIMAL,
+    admin_fee: DataTypes.DECIMAL,
+    total_amount: DataTypes.DECIMAL,
+    status: DataTypes.STRING,
+    expired_at: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Payments',
+    tableName: 'payments',
+    underscored: true,
+    updatedAt: false,
+  });
+  return Payments;
+};
