2e414ee6dc3971e6a2928bf47749d85e3b232616
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-16 19:31:38 +0700

    feat: migration sequelize user orders

---
+++ src/migrations/20260816111335-create-orders.cjs
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user_orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      address_id: {
+        type: Sequelize.INTEGER
+      },
+      total_price: {
+        type: Sequelize.DECIMAL(10, 2)
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('user_orders');
+  }
+};
