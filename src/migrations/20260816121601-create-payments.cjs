461ecb962b47a529a930fad40efc69123df53358
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-16 19:31:09 +0700

    chore: migrate create payment

---
+++ src/migrations/20260816121601-create-payments.cjs
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('payments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      order_id: {
+        type: Sequelize.INTEGER
+      },
+      payment_method_id: {
+        type: Sequelize.INTEGER
+      },
+      va_number: {
+        type: Sequelize.STRING
+      },
+      amount: {
+        type: Sequelize.DECIMAL
+      },
+      admin_fee: {
+        type: Sequelize.DECIMAL
+      },
+      total_amount: {
+        type: Sequelize.DECIMAL
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      expired_at: {
+        type: Sequelize.DATE
+      },
+      paid_at: { 
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('payments');
+  }
+};
