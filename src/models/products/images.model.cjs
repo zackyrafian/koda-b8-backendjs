6f976093518f9df9fc36fed994a6d0dda734fd96
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-16 16:26:23 +0700

    feat: add models sequelize product images

---
+++ src/models/products/images.model.cjs
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ProductImages extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      ProductImages.belongsTo(models.Products, { 
+        foreignKey: 'product_id', 
+        as: 'products'
+      })
+    }
+  }
+  ProductImages.init({
+    product_id: DataTypes.INTEGER,
+    url: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'ProductImages',
+    tableName: 'product_images',
+    underscored: true,
+    updatedAt: false,
+  });
+  return ProductImages;
+};
