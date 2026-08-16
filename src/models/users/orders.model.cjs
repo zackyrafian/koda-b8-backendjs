159844d1dfc8477dfc1938ea06e40fdc7be4fc9d
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-16 19:31:55 +0700

    feat: orders model sequelize

---
+++ src/models/users/orders.model.cjs
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class UserOrders extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+
+    }
+  }
+  UserOrders.init({
+    user_id: DataTypes.INTEGER,
+    address_id: DataTypes.INTEGER,
+    total_price: DataTypes.DECIMAL(10, 2),
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'UserOrders',
+    tableName: 'user_orders', 
+    underscored: true,
+  });
+  return UserOrders;
+};
