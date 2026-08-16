98aba2d176874cae0630592aba7b3384b98f8cdf
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-16 14:19:06 +0700

    feat: add user carts migraration sequelize

---
+++ src/migrations/20260816062945-create-user-cart.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user_carts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      product_id: {
+        type: Sequelize.INTEGER
+      },
+      quantity: {
+        type: Sequelize.INTEGER
+      },
+      variant: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('user_carts');
+  }
+};
---
+++ src/models/users/cart.model.cjs
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class UserCart extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      UserCart.belongsTo(models.Products, { 
+        foreignKey: 'product_id', 
+        as: 'products'
+      })
+    }
+  }
+  UserCart.init({
+    user_id: DataTypes.INTEGER,
+    product_id: DataTypes.INTEGER,
+    quantity: DataTypes.INTEGER,
+    variant: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'UserCart',
+    tableName: 'user_carts', 
+    underscored: true
+  });
+  return UserCart;
+};
