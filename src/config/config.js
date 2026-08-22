d516530f33dc77fa803048d83b2bb4f69fe40764
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-23 00:58:06 +0700

    chore: change config.json to js

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,11 @@
-{
+export default {
   "development": {
     "username": "postgres",
     "password": "admin",
     "database": "belimudah",
-    "host": "postgres",
-    "dialect": "postgres"
+    "host": "127.0.0.1",
+    "dialect": "postgres", 
+    "logging": false
   },
   "test": {
     "username": "root",
@@ -14,10 +15,10 @@
     "dialect": "postgres"
   },
   "production": {
-    "username": "postgres",
-    "password": "admin",
-    "database": "belimudah",
-    "host": "postgres",
+    "username": process.env.DB_NAME || "postgres",
+    "password": process.env.DB_NAME || "admin",
+    "database": process.env.DB_NAME|| "belimudah",
+    "host": process.env.DB_HOST || "localhost",
     "dialect": "postgres"
   }
 }
--- src/models/index.js
+++ src/models/index.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.json')[env];
+const config = require('../config/config.js')[env];
 const db = {};
 
 let sequelize;
