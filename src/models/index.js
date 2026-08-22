40c72a81d5d90ea324285b7ea80c3e1347b644dd
Zacky Rafian <imftcy@gmail.com>
2026-08-23 01:36:41 +0700

    fix: rename config.js to config.cjs for CommonJS compatibility

--- src/models/index.js
+++ src/models/index.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.js')[env];
+const config = require('../config/config.cjs')[env];
 const db = {};
 
 let sequelize;
