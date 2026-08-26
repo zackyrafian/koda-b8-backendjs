da85a106714d9c91767b472a5610dccf9c51f9f1
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-26 14:30:03 +0700

    refactor: replace seeders

--- src/seeders/20260825114322-payment-methods-seeder.cjs
+++ src/seeders/20260825114322-payment-methods-seeder.cjs
@@ -1,25 +1,25 @@
 'use strict';
 
 const paymentMethods = [
-  ['BCA_VA', 'BCA Virtual Account', 'VA', '88810', 15],
-  ['BNI_VA', 'BNI Virtual Account', 'VA', '988', 12],
-  ['MANDIRI_VA', 'Mandiri Virtual Account', 'VA', '89508', 13],
-  ['BRI_VA', 'BRI Virtual Account', 'VA', '128', 15],
+  ['BCA_VA', 'BCA Virtual Account', 'VA', '88810', 15, true],
+  ['BNI_VA', 'BNI Virtual Account', 'VA', '988', 12, true],
+  ['MANDIRI_VA', 'Mandiri Virtual Account', 'VA', '89508', 13, true],
+  ['BRI_VA', 'BRI Virtual Account', 'VA', '128', 15, true],
 ];
 
 /** @type {import('sequelize-cli').Seeder} */
 module.exports = {
   async up(queryInterface) {
-    const cols = 5;
+    const cols = 6;
     const values = paymentMethods
       .map((_, r) => `(${Array.from({ length: cols }, (_, c) => `$${r * cols + c + 1}`).join(', ')})`)
       .join(', ');
     const binds = paymentMethods.flat();
 
     await queryInterface.sequelize.query(
-      `INSERT INTO payment_methods (code, name, type, va_code, va_length)
-       SELECT v.code, v.name, v.type, v.va_code, v.va_length::int
-       FROM (VALUES ${values}) AS v(code, name, type, va_code, va_length)
+      `INSERT INTO payment_methods (code, name, type, va_code, va_length, is_active)
+       SELECT v.code, v.name, v.type, v.va_code, v.va_length::int, v.is_active::boolean
+       FROM (VALUES ${values}) AS v(code, name, type, va_code, va_length, is_active)
        WHERE NOT EXISTS (SELECT 1 FROM payment_methods pm WHERE pm.code = v.code)`,
       { bind: binds }
     );
